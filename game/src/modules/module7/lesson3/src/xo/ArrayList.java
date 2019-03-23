f4476c89e813c2b105b2d35e1595f4d08fc5c11a
kirill <kymbrik3@rambler.ru>
2019-03-23 16:59:36 +0300

    тмп

--- game/src/modules/module7/lesson3/src/xo/ArrayList.java
+++ game/src/modules/module7/lesson3/src/xo/ArrayList.java
@@ -2,36 +2,60 @@ package xo;
 
 class ArrayList implements IList {
     // BEGIN (write your solution here)
-
+    private Object[] list = new Object[100];
+    private int size = 0;
     // END
 
     public synchronized void add(final Object element) {
         // BEGIN (write your solution here)
+        list[size] = element;
 
+        size++;
+
+        if(size % 100 == 0)
+        {
+            Object[] newList = new Object[size+100];
+
+            for (int i =0 ; i< size; i ++)
+            {
+                newList[i] = list[i];
+            }
+            list = newList;
+        }
         // END
     }
 
     public synchronized void removeLast() {
         // BEGIN (write your solution here)
+        if(size > 0)
+        {
+            this.list[size - 1] = null;
+            size--;
+        }
 
         // END
     }
 
     public int size() {
         // BEGIN (write your solution here)
-
+        return size;
         // END
     }
 
     public Object get(final int index) {
         // BEGIN (write your solution here)
 
+        if(index < size && index >= 0 )
+        {
+            return this.list[index];
+        }
+        return null;
         // END
     }
 
     public synchronized void set(final int index, final Object object) {
         // BEGIN (write your solution here)
-
+        this.list[index] = object;
         // END
     }
 }
